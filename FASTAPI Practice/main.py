8527068df5dd34ef500054f7bfc51e948ff94388
shaistaDev7 <shaistazulfiqar61@gmail.com>
2026-05-29 22:55:54 +0500

    Path and Query in FastAPI

--- FASTAPI Practice/main.py
+++ FASTAPI Practice/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 def load_data():
     with open("patients.json", 'r') as f:
@@ -18,10 +18,38 @@ def view():
     return data
 
 @app.get("/Patient/{patient_id}")
-def View_patient(patient_id: str):
+def View_patient(patient_id: str=Path(..., example="P001")):
     data = load_data()
     
     if patient_id in data:
         return data[patient_id]
     
-    return {"Error": "Data NOT Found"}
+    raise HTTPException(status_code=404, detail="Data NOT Found")
+
+
+@app.get("/Sorted")
+def Sorted(
+    sorted_by: str = Query(..., description="Sort by height, weight, or bmi"),
+    Order_by: str = Query("asc", description="Order: asc or desc")
+):
+    
+    valid_fields = ["height", "weight", "bmi"]
+    
+    # Check valid field
+    if sorted_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f"Select from {valid_fields}")
+    
+    # Check order
+    if Order_by not in ["asc", "desc"]:
+        raise HTTPException(status_code=400, detail="Select from ['asc', 'desc']")
+    
+    data = load_data()
+    
+    # Sorting
+    reverse = True if Order_by == "desc" else False
+    
+    sorted_data = sorted(data, key=lambda x: x[sorted_by], reverse=reverse)
+    
+    return sorted_data
+    
+    
